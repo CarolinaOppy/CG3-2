bebc7d16db7abf9c4b6e77897ff458fe884f0ee6
carocosta87 <carocosta87@gmail.com>
2018-08-07 11:22:17 -0500

    Finished

---
+++ Guess my number/Guess my number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Guess_my_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //establishing program and asking for variable.//
+            Console.WriteLine("Hello, let's play a game!");
+            Console.WriteLine("Guess a number between 1-10.");
+            var number = Console.ReadLine();
+            
+            //inline statement of the boolean//
+            string message = (number == "8") ? "You win!" : "You lose." ;
+
+            //output//
+            Console.WriteLine(message);
+            Console.ReadLine();
+            
+            
+
+         
+        }
+    }
+}
